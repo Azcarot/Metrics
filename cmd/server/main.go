d320636d6a21d404e03ba389b9bbea29717cfd14
Volkov Alexey <Azcarot387@gmail.com>
2023-12-02 16:22:09 +0300

    Правка аллокации памяти по слайсы

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -40,56 +40,68 @@ func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
 func PostReq(res http.ResponseWriter, req *http.Request) {
 	method := req.Method
 	requiredcontenttype := []string{"text/plain"}
-	if method == http.MethodPost {
-		header := req.Header
-		if header["Contert-Type"][0] != requiredcontenttype[0] {
-			res.WriteHeader(http.StatusUnsupportedMediaType)
-			return
-		}
+		if method == http.MethodPost {
+		ctype := req.Header.Get("Content-type") 
+		if ctype != requiredcontenttype[0] {
+		 	res.WriteHeader(http.StatusUnsupportedMediaType)
+		  	return
+		 }
 		urlmap := make(map[string]string)
 		reqpaths := []string{"subaction", "mettype", "metname", "metvalue"}
 		url := strings.Split(req.URL.Path, "/")
 		for i := range reqpaths {
 			urlmap[reqpaths[i]] = url[i+1]
-			if reqpaths[i] == "subaction" && url[i+1] != "update" {
-				res.WriteHeader(http.StatusBadRequest)
-				return
-			}
-			if len(url[i+1]) == 0 && reqpaths[i] == "metname" {
-				res.WriteHeader(http.StatusNotFound)
-				return
-			}
+			 if reqpaths[i] == "subaction" && url[i+1] != "update" {
+			 	res.WriteHeader(http.StatusBadRequest)
+			 	return
+			 }
+			 if len(url[i+1]) == 0 && reqpaths[i] == "metname" {
+			 	res.WriteHeader(http.StatusNotFound)
+			 	return
+			 }
 		}
 		var mem MemStorage
 		switch urlmap["mettype"] {
 		case "float64":
-			if value, err := strconv.ParseFloat(urlmap["metvalue"], 64); err == nil {
-				mem.floatmem["metname"] = value
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
-		case "gauge":
-			if value, err := strconv.Atoi(urlmap["metvalue"]); err == nil {
-				mem.gaugemem["metname"] = gauge(value)
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
+		  	if value, err := strconv.ParseFloat(urlmap["metvalue"], 64); err == nil {
+		  		mem.floatmem["metname"] = value
+				  res.WriteHeader(http.StatusOK)
+				  return
+		  	} else {
+		  		res.WriteHeader(http.StatusBadRequest)
+		  	}
+		  case "gauge":
+		  	if value, err := strconv.Atoi(urlmap["metvalue"]); err == nil {
+		  		mem.gaugemem["metname"] = gauge(value)
+				  res.WriteHeader(http.StatusOK)
+				  return
+		  	} else {
+		  		res.WriteHeader(http.StatusBadRequest)
+		  	}
 		case "counter":
-			if value, err := strconv.Atoi(urlmap["metvalue"]); err == nil {
-				mem.countermem["metname"] = append(mem.countermem["metname"], counter(value))
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
-		case "int64":
-			if value, err := strconv.ParseInt(urlmap["metvalue"], 0, 64); err == nil {
+		if value, err := strconv.Atoi(urlmap["metvalue"]); err == nil {
+			mem.countermem = make(map[string][]counter, 1)
+			mem.countermem["metname"]= append(mem.countermem["metname"], counter(value))
+			res.WriteHeader(http.StatusOK)
+		 return
+		 	} else {
+		  		res.WriteHeader(http.StatusBadRequest)
+				return
+		  	}
+		  case "int64":
+		  	if value, err := strconv.ParseInt(urlmap["metvalue"], 0, 64); err == nil {
+				mem.int64mem = make(map[string][]int64, 1)
 				mem.int64mem["metname"] = append(mem.int64mem["metname"], value)
-			} else {
-				res.WriteHeader(http.StatusBadRequest)
-			}
-		default:
-			res.WriteHeader(http.StatusBadRequest)
-		}
-		res.WriteHeader(http.StatusOK)
+				res.WriteHeader(http.StatusOK)
+		 return
+		  	} else {
+		  		res.WriteHeader(http.StatusBadRequest)
+		  	}
+	    default:
+	  	res.WriteHeader(http.StatusBadRequest)
+		return
+		  }
+		 
 
 	} else {
 		res.WriteHeader(http.StatusMethodNotAllowed)
