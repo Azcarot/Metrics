b9042bb93adb19b35ed43c0401af4c72daa96f68
Volkov Alexey <Azcarot387@gmail.com>
2023-12-04 09:35:34 +0300

    Имена пакетов

---
+++ cmd/agent/agent.go
@@ -0,0 +1,21 @@
+package agent
+
+import (
+	"agent/measure"
+	"agent/postmetrics"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	var metric measure.MemStorage
+	metric = measure.GetMetrics(metric)
+	urls := postmetrics.Makepath(metric)
+	var resp *http.Response
+	for _, url := range urls {
+		resp = postmetrics.PostMetrics(url)
+	}
+	fmt.Sprintf("metric no longer supported", resp)
+
+}
