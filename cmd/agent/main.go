b0b2685bb8c339b7d92eb6f4f628a886738af9ac
Volkov Alexey <Azcarot387@gmail.com>
2023-12-04 10:10:30 +0300

    таймер

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -5,17 +5,24 @@ import (
 	"agent/postmetrics"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 
 	var metric measure.MemStorage
-	metric = measure.GetMetrics(metric)
-	urls := postmetrics.Makepath(metric)
-	var resp *http.Response
-	for _, url := range urls {
-		resp = postmetrics.PostMetrics(url)
+	counter := 0
+	for {
+		metric = measure.GetMetrics(metric)
+		time.Sleep(2 * time.Second)
+		counter += 2
+		if counter%10 == 0 {
+			urls := postmetrics.Makepath(metric)
+			var resp *http.Response
+			for _, url := range urls {
+				resp = postmetrics.PostMetrics(url)
+			}
+			fmt.Sprintf("metric no longer supported", resp)
+		}
 	}
-	fmt.Sprintf("metric no longer supported", resp)
-
 }
