bebcf92d7ff7583aa60e8a21c8dd18a21e4bc48b
Volkov Alexey <Azcarot387@gmail.com>
2023-12-28 22:12:09 +0300

    Content-Type

--- cmd/server/handlers/jsonhandler.go
+++ cmd/server/handlers/jsonhandler.go
@@ -27,6 +27,7 @@ func GzipHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Add("Content-Type", "text/plain")
 			next.ServeHTTP(w, r)
 			return
 		}
